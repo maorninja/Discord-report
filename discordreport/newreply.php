a6df1004c9a53de8e9e508e91b98ce42e11a6e82
maorninja <maorninja@outlook.com>
2017-08-17 11:10:58 -0700

    Update newreply.php
    Fix discord issues

--- discordreport/newreply.php
+++ discordreport/newreply.php
@@ -2,21 +2,18 @@
 
 if ($isHidden) return;
 
-$c1 = ircColor(Settings::pluginGet("color1"));
-$c2 = ircColor(Settings::pluginGet("color2"));
-
 $thename = $loguser["name"];
 if($loguser["displayname"])
 	$thename = $loguser["displayname"];
 	
-$fpage = ircForumPrefix($forum);
+$fpage = DiscordForumPrefix($forum);
 $link = getServerDomainNoSlash().actionLink("post", $pid);
 
-ircReport("\003".$c2."New reply by\003$c1 "
-	.ircUserColor($thename, $loguser['sex'], 0)
-	."\003$c2: \003$c1"
+DiscordReport("New reply by "
+	.$thename
+	.": "
 	.$thread["title"]
-	."\003$c2 (".$fpage.$forum["title"].")"
+	." (".$fpage.$forum["title"].")"
 	." -- "
 	.$link
 	);
